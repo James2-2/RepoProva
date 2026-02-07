b8a61047327805f4618bfcee15e1c06c9351fe36
James2-2 <150335369+James2-2@users.noreply.github.com>
2026-02-07 09:15:59 +0100

    Update script.php

--- script.php
+++ script.php
@@ -2,9 +2,9 @@
 $var = 2;
 echo $var;
 $db = new PDO(
-    "mysql:host=192.168.60.144;dbname=giacomo_redi_itis;charset=utf8mb4",
-    "giacomo_redi",
-    "strariperebbero.dassistenza.",
+    "mysql:host=;dbname=;charset=utf8mb4",
+    "",
+    "",
     [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, // PHP Data Object, una tupla diventa un oggetto
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Gli errori di sql vengono gestiti come eccezioni
